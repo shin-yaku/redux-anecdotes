f34934062856a806d968a7568d135a6be4179f51
shin-yaku <foo@example.com>
2021-07-23 12:36:50 +0900

    add 6.4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { upvote } from './reducers/anecdoteReducer'
+import { upvote, getId } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -9,6 +9,21 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(upvote(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: {
+        content,
+        id: getId(),
+        votes: 0
+      }
+    })
   }
 
   return (
@@ -26,8 +41,8 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
         <button>create</button>
       </form>
     </div>
