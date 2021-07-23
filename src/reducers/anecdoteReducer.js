c84f5f8682c99f5031041cfe6df19b1d19907b4e
shin-yaku <foo@example.com>
2021-07-23 12:25:25 +0900

    add 6.3

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -19,11 +19,35 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
+export const upvote = (id) => {
+  return {
+    type: 'VOTE',
+    data: {
+      id: id
+    }
+  }
+}
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdotesToChange = state.find(a => a.id === id)
+      const changedAnecdotes = {
+        ...anecdotesToChange,
+        votes: anecdotesToChange.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdotes
+      )
+
+    default:
+      return state
+  }
 }
+
 
 export default reducer
