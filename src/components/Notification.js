06f178c2ef068b074bd26b7a2247dd01ca98b17b
shin-yaku <foo@example.com>
2021-07-31 18:35:47 +0900

    add 6.19

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const style = () => {
     if (notification.visible) {
       return {
@@ -23,4 +23,11 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
